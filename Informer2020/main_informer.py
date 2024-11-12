4f74c4cbf28c635d5dca895ce8616ae9536766ea
jchanxtarov <shi3mizu8-r@fuji.waseda.jp>
2024-11-12 22:10:46 +0000

    [fix] sorting NaturalGas data

--- Informer2020/main_informer.py
+++ Informer2020/main_informer.py
@@ -226,17 +226,16 @@ def parse_args(arg_str: Optional[str] = None) -> argparse.Namespace:
         # see also: https://www.kaggle.com/datasets/ayan2002/price-and-withdrawals-of-natural-gas
         "NaturalGas": {
             "data": "combined_data.csv",
-            "T": "price_value",  # NOTE: administered_price, actual_wdl_gj, actual_inj_gj なども使えるかも？
-            "M": [7, 7, 7],  # TODO: setting
+            "T": "actual_wdl_gj",  # NOTE: price_value, actual_wdl_gj, actual_inj_gj なども使えるかも
+            "M": [5, 5, 5],  # TODO: setting
             "S": [1, 1, 1],  # TODO: setting
-            "MS": [7, 7, 1],  # TODO: setting
+            "MS": [5, 5, 1],  # TODO: setting
         },
     }
     if args.data in data_parser.keys():
         data_info = data_parser[args.data]
         args.data_path = data_info["data"]
         args.target = data_info["T"]
-        # M, S, MS
         args.enc_in, args.dec_in, args.c_out = data_info[args.features]
 
     args.s_layers = [int(s_l) for s_l in args.s_layers.replace(" ", "").split(",")]
